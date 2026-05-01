e82c33158f3d397c74c5908346dd2cc9856bb770
m-ariza <m.ariza214@gmail.com>
2026-05-01 05:48:59 -0400

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'rt321-calc-v1';
+const ASSETS_TO_CACHE =[
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install Event: Cache the local files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Fetch Event: Serve from cache first (fully offline), then try network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Event: Cleanup older versions of caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
